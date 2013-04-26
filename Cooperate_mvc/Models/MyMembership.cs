ccbacc111fb542bad5549ca46b8e6a80aecdb94c
Łukasz <lukasz.strz91@gmail.com>
2013-04-26 09:58:27 +0200

    membership provider

--- Cooperate_mvc/Models/MyMembership.cs
+++ Cooperate_mvc/Models/MyMembership.cs
@@ -3,9 +3,146 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
+using Cooperate_mvc;
+using System.Text;
+using System.Security.Cryptography;
 
-namespace Projekt_1
+namespace Cooperate_mvc.Models
 {
+    /// <summary>
+    /// Typ hashowania.
+    /// </summary>
+    public enum HashType : int
+    {
+        MD5,
+        SHA1,
+        SHA256,
+        SHA512
+    }
+
+    /// <summary>
+    /// Klasa udostępniająca metody hashowania.
+    /// </summary>
+    public class Hash
+    {
+        private Hash() { }
+
+        /// <summary>
+        /// Hashuje zadany tekst.
+        /// </summary>
+        /// <param name="text">Tekst do hashowania.</param>
+        /// <param name="hashType">Typ hashowania.</param>
+        /// <returns>Hash podanego tekstu.</returns>
+        public static string GetHash(string text, HashType hashType)
+        {
+            string hashString;
+            switch (hashType)
+            {
+                case HashType.MD5:
+                    hashString = GetMD5(text);
+                    break;
+                case HashType.SHA1:
+                    hashString = GetSHA1(text);
+                    break;
+                case HashType.SHA256:
+                    hashString = GetSHA256(text);
+                    break;
+                case HashType.SHA512:
+                    hashString = GetSHA512(text);
+                    break;
+                default:
+                    hashString = "Invalid Hash Type";
+                    break;
+            }
+            return hashString;
+        }
+
+        /// <summary>
+        /// Sprawdza, czy tekst jest zgodny z podanym hashem.
+        /// </summary>
+        /// <param name="original">Tekst</param>
+        /// <param name="hashString">Hash, z którym porównujemy tekst</param>
+        /// <param name="hashType">Typ hashowania.</param>
+        /// <returns></returns>
+        public static bool CheckHash(string original, string hashString, HashType hashType)
+        {
+            string originalHash = GetHash(original, hashType);
+            return (originalHash == hashString);
+        }
+
+        #region Funkcje hashujące
+
+        private static string GetMD5(string text)
+        {
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] hashValue;
+            byte[] message = UE.GetBytes(text);
+
+            MD5 hashString = new MD5CryptoServiceProvider();
+            string hex = "";
+
+            hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue)
+            {
+                hex += String.Format("{0:x2}", x);
+            }
+            return hex;
+        }
+
+        private static string GetSHA1(string text)
+        {
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] hashValue;
+            byte[] message = UE.GetBytes(text);
+
+            SHA1Managed hashString = new SHA1Managed();
+            string hex = "";
+
+            hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue)
+            {
+                hex += String.Format("{0:x2}", x);
+            }
+            return hex;
+        }
+
+        private static string GetSHA256(string text)
+        {
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] hashValue;
+            byte[] message = UE.GetBytes(text);
+
+            SHA256Managed hashString = new SHA256Managed();
+            string hex = "";
+
+            hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue)
+            {
+                hex += String.Format("{0:x2}", x);
+            }
+            return hex;
+        }
+
+        private static string GetSHA512(string text)
+        {
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] hashValue;
+            byte[] message = UE.GetBytes(text);
+
+            SHA512Managed hashString = new SHA512Managed();
+            string hex = "";
+
+            hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue)
+            {
+                hex += String.Format("{0:x2}", x);
+            }
+            return hex;
+        }
+
+        #endregion
+    }
+
     public class MyMembership : MembershipProvider
     {
         string appname = "MyMembershipProvider";
@@ -149,58 +286,13 @@ namespace Projekt_1
 
         public override bool ValidateUser(string username, string password)
         {
-            using (db_BigbrainDataContext db = new db_BigbrainDataContext())
+            using (compact_dbEntities db = new compact_dbEntities())
             {
-                bool valid;
-
-                List<P1_User> users = (from u in db.P1_Users
-                                 where u.User_login.Equals(username)
-                                 select u).ToList();
-
-                if (users.Count == 0)
-                    return false;
-                if (users.Count > 1)
-                    throw new System.Exception("Błąd bazy danych.");
-
-                if (users[0].User_IsLocked)
-                    valid=false;
-                else
-                {
-                    valid = Hash.Hash.CheckHash(password, users[0].User_password, Hash.HashType.SHA512); // sprawdzanie zgodności hasła
-                    if(!valid)
-                    {
-                        List<P1_LoginAttempt> attempts = users[0].P1_LoginAttempts.ToList();
-                        if(attempts.Count>=2 && (!attempts[attempts.Count-1].LoginAttempt_success && !attempts[attempts.Count-2].LoginAttempt_success)) // blokowanie usera w przypadku trzeciego błędnego logowania
-                            users[0].User_IsLocked=true;
-                    }
-                }
-
-                P1_LoginAttempt attempt = new P1_LoginAttempt();
-                attempt.LoginAttempt_date = DateTime.Now;
-                attempt.LoginAttempt_success=valid;
-                //attempt.User_login=username;
-                users[0].P1_LoginAttempts.Add(attempt);
-                db.SubmitChanges();
-                return valid;
-            }
-        }
-
-        public static bool IsLocked(string username)
-        {
-            using (db_BigbrainDataContext db = new db_BigbrainDataContext())
-            {
-                List<P1_User> users = (from u in db.P1_Users
-                                       where u.User_login.Equals(username)
-                                       select u).ToList();
-
-                if (users.Count == 0)
-                {
-                    throw new ArgumentException("Brak użytkownika o podanej nazwie.");
-                }
-                else if (users.Count > 1)
-                    throw new System.Exception("Błąd bazy danych.");
-
-                return users[0].User_IsLocked;
+                User user = (from u in db.Users
+                                 where u.User_email.Equals(username) && Hash.CheckHash(password,u.User_pass,HashType.SHA512)
+                                 select u).SingleOrDefault();
+                
+                return (user != null);
             }
         }
     }
