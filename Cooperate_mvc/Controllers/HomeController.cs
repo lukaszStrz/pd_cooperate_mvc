689a70af1846819a7c95710e3cef2ca70f642e14
Łukasz <lukasz.strz91@gmail.com>
2013-06-17 00:05:59 +0200

    home redirect for authenticated users

--- Cooperate_mvc/Controllers/HomeController.cs
+++ Cooperate_mvc/Controllers/HomeController.cs
@@ -4,11 +4,14 @@ namespace Cooperate_mvc.Controllers
 {
     public class HomeController : Controller
     {
+
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Tasks");
             return View();
         }
     }
